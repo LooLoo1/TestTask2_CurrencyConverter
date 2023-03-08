f98caecc4a1f4aa94e7b63be980aa8773ddeae66
Vitaliy Petriv <vamnir132132@gmail.com>
2023-03-08 18:07:09 +0200

    Stylyng select

--- src/components/CurrencyInput/index.jsx
+++ src/components/CurrencyInput/index.jsx
@@ -1,14 +1,41 @@
-import './CurrencyInput.scss';
+import Select from "react-select";
+import "./CurrencyInput.scss";
 
 function CurrencyInput(props) {
+  const options = props.currencies.map((currency) => {
+    return { value: currency, label: currency };
+  });
+
+  const customStyles = {
+    option: (defaultStyles) => ({
+      ...defaultStyles,
+      color: "#121e62",
+      backgroundColor: "white",
+    }),
+
+    control: (defaultStyles) => ({
+      ...defaultStyles,
+      backgroundColor: "white",
+      padding: "10px",
+      border: "none",
+      boxShadow: "none",
+    }),
+    singleValue: (defaultStyles) => ({ ...defaultStyles, color: "#121e62" }),
+  };
+
   return (
     <div className="group">
-      <input type="text" value={props.amount} onChange={ev => props.onAmountChange(ev.target.value)} />
-      <select value={props.currency} onChange={ev => props.onCurrencyChange(ev.target.value)}>
-        {props.currencies.map((currency => (
-          <option key={currency} value={currency}>{currency}</option>
-        )))}
-      </select>
+      <input
+        type="text"
+        value={props.amount}
+        onChange={(ev) => props.onAmountChange(ev.target.value)}
+      />
+      <Select
+        options={options}
+        styles={customStyles}
+        defaultValue={{ label: props.currency, value: props.currency }}
+        onChange={(ev) => props.onCurrencyChange(ev.value)}
+      />
     </div>
   );
 }
